05cbd43693e41494c24b2391bf6af2e285fe15e8
Chokaaa <solidaritythehydro@gmail.com>
2021-07-24 12:24:28 +0200

    Login page and Mojang authentication

--- src/main/java/fr/chokaaa/launcher/Launcher.java
+++ src/main/java/fr/chokaaa/launcher/Launcher.java
@@ -1,9 +1,16 @@
 package fr.chokaaa.launcher;
 
 import fr.chokaaa.launcher.ui.PanelManager;
+import fr.chokaaa.launcher.ui.panels.pages.Login;
 import fr.chokaaa.launcher.utils.Helpers;
 import fr.flowarg.flowlogger.ILogger;
 import fr.flowarg.flowlogger.Logger;
+import fr.litarvan.openauth.AuthPoints;
+import fr.litarvan.openauth.AuthenticationException;
+import fr.litarvan.openauth.Authenticator;
+import fr.litarvan.openauth.model.AuthProfile;
+import fr.litarvan.openauth.model.response.RefreshResponse;
+import fr.theshark34.openlauncherlib.util.Saver;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -13,19 +20,64 @@ public class Launcher extends Application {
     private PanelManager panelManager;
     private static Launcher instance;
     private final ILogger logger;
-    private final File LauncherDir = Helpers.generatGamePath("FuFX");
+    private final File launcherDir = Helpers.generatGamePath(".Irkelia");
+    private final Saver saver;
+    private AuthProfile authProfile = null;
 
     public Launcher() {
         instance = this;
-        this.logger = new Logger("FuFX", new File(this.LauncherDir, "launcher.log"));
-        this.LauncherDir.mkdirs();
+        this.logger = new Logger("[Irkelia]", new File(this.launcherDir, "launcher.log"));
+        if (!this.launcherDir.exists()) {
+            if (!this.launcherDir.mkdir()) {
+                this.logger.err("Unable to create launcher folder");
+            }
+        }
+
+        saver = new Saver(new File(launcherDir, "config.properties"));
+        saver.load();
     }
 
     @Override
     public void start(Stage stage) {
-        this.logger.info("Starting Launcher");
+        this.logger.info("Starting launcher");
         this.panelManager = new PanelManager(this, stage);
         this.panelManager.init();
+
+        if (this.isUserAlreadyLoggedIn()) {
+            logger.info("Hello " + authProfile.getName());
+        } else {
+            this.panelManager.showPanel(new Login());
+        }
+    }
+
+    public boolean isUserAlreadyLoggedIn() {
+        if (saver.get("accessToken") != null && saver.get("clientToken") != null) {
+            Authenticator authenticator = new Authenticator(Authenticator.MOJANG_AUTH_URL, AuthPoints.NORMAL_AUTH_POINTS);
+
+            try {
+                RefreshResponse response  = authenticator.refresh(saver.get("accessToken"), saver.get("clientToken"));
+                saver.set("accessToken", response.getAccessToken());
+                saver.set("clientToken", response.getClientToken());
+                saver.save();
+                this.authProfile = response.getSelectedProfile();
+
+                return true;
+            } catch (AuthenticationException ignored) {
+                saver.remove("accessToken");
+                saver.remove("clientToken");
+                saver.save();
+            }
+        }
+
+        return false;
+    }
+
+    public void setAuthProfile(AuthProfile authProfile) {
+        this.authProfile = authProfile;
+    }
+
+    public AuthProfile getAuthProfile() {
+        return authProfile;
     }
 
     public ILogger getLogger() {
@@ -35,4 +87,9 @@ public class Launcher extends Application {
     public static Launcher getInstance() {
         return instance;
     }
+
+    public Saver getSaver() {
+        return saver;
+    }
+
 }
--- src/main/java/fr/chokaaa/launcher/ui/PanelManager.java
+++ src/main/java/fr/chokaaa/launcher/ui/PanelManager.java
@@ -4,9 +4,12 @@ import com.goxr3plus.fxborderlessscene.borderless.BorderlessScene;
 import fr.chokaaa.launcher.Launcher;
 import fr.chokaaa.launcher.ui.panel.IPanel;
 import fr.chokaaa.launcher.ui.panels.partials.TopBar;
+import fr.flowarg.flowcompat.Platform;
 import javafx.geometry.VPos;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -15,9 +18,7 @@ public class PanelManager {
     private final Launcher launcher;
     private final Stage stage;
     private GridPane layout;
-    private final TopBar topBar = new TopBar();
     private final GridPane contentPane = new GridPane();
-
 
     public PanelManager(Launcher launcher, Stage stage) {
         this.launcher = launcher;
@@ -25,39 +26,53 @@ public class PanelManager {
     }
 
     public void init() {
-        this.stage.setTitle("Irkelia");
+        this.stage.setTitle("Irkelia Launcher");
         this.stage.setMinWidth(854);
         this.stage.setMinHeight(480);
         this.stage.setWidth(1280);
         this.stage.setHeight(720);
-        this.stage.initStyle(StageStyle.UNDECORATED);
         this.stage.centerOnScreen();
         this.stage.getIcons().add(new Image("images/icon.png"));
 
         this.layout = new GridPane();
 
-        BorderlessScene scene = new BorderlessScene(this.stage, StageStyle.UNDECORATED, this.layout);
-        scene.setResizable(true);
-        scene.setMoveControl(this.topBar.getLayout());
-        scene.removeDefaultCSS();
+        if (Platform.isOnLinux()) {
+            Scene scene = new Scene(this.layout);
+            this.stage.setScene(scene);
+        } else {
+            this.stage.initStyle(StageStyle.UNDECORATED);
 
-        this.stage.setScene(scene);
+            TopBar topBar = new TopBar();
+            BorderlessScene scene = new BorderlessScene(this.stage, StageStyle.UNDECORATED, this.layout);
+            scene.setResizable(true);
+            scene.setMoveControl(topBar.getLayout());
+            scene.removeDefaultCSS();
+
+            this.stage.setScene(scene);
+
+            RowConstraints topPaneContraints = new RowConstraints();
+            topPaneContraints.setValignment(VPos.TOP);
+            topPaneContraints.setMinHeight(25);
+            topPaneContraints.setMaxHeight(25);
+            this.layout.getRowConstraints().addAll(topPaneContraints, new RowConstraints());
+            this.layout.add(topBar.getLayout(), 0, 0);
+            topBar.init(this);
+        }
+
+        this.layout.add(this.contentPane, 0, 1);
+        GridPane.setVgrow(this.contentPane, Priority.ALWAYS);
+        GridPane.setHgrow(this.contentPane, Priority.ALWAYS);
 
         this.stage.show();
-
-        RowConstraints topPaneConstraint = new RowConstraints();
-        topPaneConstraint.setValignment(VPos.TOP);
-        topPaneConstraint.setMinHeight(25);
-        topPaneConstraint.setMaxHeight(25);
-        this.layout.getRowConstraints().addAll(topPaneConstraint, new RowConstraints());
-        this.layout.add(this.topBar.getLayout(), 0, 0);
-        this.topBar.init(this);
-
     }
 
     public void showPanel(IPanel panel) {
         this.contentPane.getChildren().clear();
         this.contentPane.getChildren().add(panel.getLayout());
+        if (panel.getStylesheetPath() != null) {
+            this.stage.getScene().getStylesheets().clear();
+            this.stage.getScene().getStylesheets().add(panel.getStylesheetPath());
+        }
         panel.init(this);
         panel.onShow();
     }
